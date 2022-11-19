46dae274e379b224c166c55119c48147b4703f22
Ali Kashev <57992892+alikashev@users.noreply.github.com>
2022-11-19 19:20:53 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
+import React from 'react';
+import { Routes, Route, Link } from 'react-router-dom';
+import { Layout, Typography, Space } from 'antd';
+
+import { Exchanges, Homepage, News, Cryptocurrencies, CryptoDetails, Navbar } from './components';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () => (
+  <div className="app">
+    <div className="navbar">
+      <Navbar />
     </div>
-  );
-}
+    <div className="main">
+      <Layout>
+        <div className="routes">
+          <Routes>
+            <Route path="/" element={<Homepage />} />
+            <Route path="/exchanges" element={<Exchanges />} />
+            <Route path="/cryptocurrencies" element={<Cryptocurrencies />} />
+            <Route path="/crypto/:coinId" element={<CryptoDetails />} />
+            <Route path="/news" element={<News />} />
+          </Routes>
+        </div>
+      </Layout>
+    </div>
+  </div>
+);
 
 export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import { button, Menu, Typography, Avatar } from 'antd';
+import { Link } from 'react-router-dom';
+import { HomeOutlined, MoneyCollectOutlined, BulbOutlined, FundOutlined, MenuOutlined } from '@ant-design/icons';
+import icon from '../img/cryptoapp.png'
+import MenuItem from 'antd/es/menu/MenuItem';
+
+
+const Navbar = () => {
+  return (
+    <div className='nav-container'>
+        <div className='logo-container'>
+            <Avatar src={icon} size='large' />
+            <Typography.Title level={2} className='logo'>
+                <Link to={"/"}>CryptoApp</Link>
+            </Typography.Title>
+        </div>
+        <Menu theme='dark'>
+            <Menu.Item icon={<HomeOutlined/>}>
+                <Link to={'/'}>Home</Link>
+            </Menu.Item>
+            <Menu.Item icon={<FundOutlined/>}>
+                <Link to={'/cryptocurrencies'}>Cryptocurrencies</Link>
+            </Menu.Item>
+            <Menu.Item icon={<BulbOutlined/>}>
+                <Link to={'/news'}>News</Link>
+            </Menu.Item>
+        </Menu>
+    </div>
+  )
+}
+
+export default Navbar
