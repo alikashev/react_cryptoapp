b4e7fcd2194d9c51686e5ddc6e2406adcd287916
Ali Kashev <57992892+alikashev@users.noreply.github.com>
2023-01-23 16:18:58 +0100

    started api integration

---
+++ src/services/cryptoApi.js
@@ -0,0 +1,24 @@
+import {createApi, fetchBaseQuery, } from '@reduxjs/toolkit/query/react'
+
+const cryptoApiHeaders = {
+    'X-RapidAPI-Key': '052b4d9ce8msh73f2b24bf3d4670p1b4c07jsn73b22b4ffa57',
+    'X-RapidAPI-Host': 'coinranking1.p.rapidapi.com'
+}
+
+const baseUrl = 'https://coinranking1.p.rapidapi.com';
+
+const createRequest = (url) => ({url, Headers: cryptoApiHeaders})
+
+export const cryptoApi = createApi({
+    reducerPath: 'cryptoApi',
+    baseQuery: fetchBaseQuery({ baseUrl }),
+    endpoints: (builder) => ({
+        getCryptos: builder.query({
+            query: () => createRequest('/coins') 
+        })
+    })
+});
+
+export const {
+    useGetCryptosQuery, 
+} = cryptoApi;
