934eb6384413998236ca669180e688212032b10f
Ali Kashev <57992892+alikashev@users.noreply.github.com>
2023-01-25 13:45:57 +0100

    fixed api error + worked on homepage

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,25 +1,39 @@
-import React from 'react'
-import millify from 'millify'
-import { Typography, Row, Col, statistic, Statistic } from 'antd'
-import { Link } from 'react-router-dom'
+import React from 'react';
+import millify from 'millify';
+import { Typography, Row, Col, statistic, Statistic } from 'antd';
+import { Link } from 'react-router-dom';
 
-import { useGetCryptosQuery } from '../services/cryptoApi'
+import { useGetCryptosQuery } from '../services/cryptoApi';
+import {Cryptocurrencies, News} from '../components';
 
 const {Title} = Typography;
 
 const Homepage = () => {
-  const {data, isFetching} = useGetCryptosQuery();
-  console.log(data)
+  const {data, isFetching} = useGetCryptosQuery(10);
+  const globalStats = data?.data?.stats; 
+
+  if(isFetching) return 'Laden...';
+
   return (
     <>
       <Title level={2} className='heading'>Globale crypto statistieken</Title>
       <Row>
-        <Col span={12}><Statistic title="Total Crypto Currencies " value={5}/></Col>
-        <Col span={12}><Statistic title="Total Exchanges" value={5}/></Col>
-        <Col span={12}><Statistic title="Total Market Cap" value={5}/></Col>
-        <Col span={12}><Statistic title="Total 24h Volume" value={5}/></Col>
-        <Col span={12}><Statistic title="Total Markets" value={5}/></Col>
+        <Col span={12}><Statistic title="Total Crypto Currencies " value={globalStats.total}/></Col>
+        <Col span={12}><Statistic title="Total Exchanges" value={millify(globalStats.totalExchanges)}/></Col>
+        <Col span={12}><Statistic title="Total Market Cap" value={millify(globalStats.totalMarketCap)}/></Col>
+        <Col span={12}><Statistic title="Total 24h Volume" value={millify(globalStats.total24hVolume)}/></Col>
+        <Col span={12}><Statistic title="Total Markets" value={millify(globalStats.totalMarkets)}/></Col>
       </Row>
+      <div className='home-heading-container'>
+        <Title level={2} className='home-title'>Top 10 Cryptocurrencies in the world</Title>
+        <Title level={2} className='show-mote'><Link to={'/cryptocurrencies'}>Show more</Link></Title>
+      </div>
+      <Cryptocurrencies simplified />
+      <div className='home-heading-container'>
+        <Title level={2} className='home-title'>Latest crypto news</Title>
+        <Title level={2} className='show-mote'><Link to={'/news'}>Show more</Link></Title>
+      </div>
+      <News simplified/>
     </>
   )
 }
--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -7,14 +7,14 @@ const cryptoApiHeaders = {
 
 const baseUrl = 'https://coinranking1.p.rapidapi.com';
 
-const createRequest = (url) => ({url, Headers: cryptoApiHeaders})
+const createRequest = (url) => ({url, headers: cryptoApiHeaders})
 
 export const cryptoApi = createApi({
     reducerPath: 'cryptoApi',
     baseQuery: fetchBaseQuery({ baseUrl }),
     endpoints: (builder) => ({
         getCryptos: builder.query({
-            query: () => createRequest('/coins') 
+            query: (count) => createRequest(`/coins?limit=${count}`), 
         })
     })
 });
